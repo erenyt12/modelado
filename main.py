52764b580a4f76a638bab2348dd759b19526bdfd
erenyt12 <erenyt12@gmail.com>
2021-10-21 20:38:34 -0300

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import modelos.fechaModulo as fecha
+
+fecha1 = fecha
+
+date = fecha
+
+
+if __name__ == '__main__':
+    fecha1.setearFecha([19,10,2021])
+    date.setearFecha([20,10,2021])
+    
+    fecha1.verFecha()
+    date.verFecha()
+
+ 
